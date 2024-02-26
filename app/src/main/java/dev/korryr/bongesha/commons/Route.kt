52fd77a19d99519f6c5ca424d2b9aa473f063eb7
Aldo Korir <kipyegonaldo@proton.me>
2024-02-26 15:41:56 +0300

    initial commit fixed conflicts:wq

---
+++ app/src/main/java/dev/korryr/bongesha/commons/Route.kt
@@ -0,0 +1,14 @@
+package dev.korryr.bongesha.commons
+
+class Route {
+
+    object Home {
+        const val Singup = "/signup/"
+        const val SignUp = "/signup/"
+        const val Login = "/login/"
+        const val List = "/list/"
+        const val Product = "/Product/"
+        const val Category = "/category/"
+
+    }
+}
