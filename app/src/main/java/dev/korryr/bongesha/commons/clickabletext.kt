cc863989571337445f47fec768d31d8c66990c13
Aldo Korir <kipyegonaldo@proton.me>
2024-02-29 16:05:34 +0300

    create button for navigation

---
+++ app/src/main/java/dev/korryr/bongesha/commons/clickabletext.kt
@@ -0,0 +1,22 @@
+package dev.korryr.bongesha.commons
+
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.sp
+import java.lang.reflect.Modifier
+
+@Composable
+fun SignInText(
+    onClick: () -> Unit,
+) {
+    Text(
+        text = AnnotatedString("Sign in"),
+        //modifier = Modifier.clickable({onClick}),
+        fontSize = 16.sp, // Adjust font size as needed
+        //style = SpanStyle(textDecoration = TextDecoration.Underline)
+    )
+}
