c9e4464e5f9163ab6a335cf7347ef2d24665db06
Aldo Korir <kipyegonaldo@proton.me>
2024-03-04 01:59:58 +0300

    upgrade to iguana ide

--- app/src/main/java/dev/korryr/bongesha/screens/signinscreen.kt
+++ app/src/main/java/dev/korryr/bongesha/screens/signinscreen.kt
@@ -3,10 +3,17 @@ package dev.korryr.bongesha.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import dev.korryr.bongesha.R
@@ -16,6 +23,14 @@ import dev.korryr.bongesha.commons.Bongatextfield
 fun BongaSignIn(
     navController: NavController
 ){
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
     Column(
         modifier = Modifier.padding(24.dp)
     ) {
@@ -24,18 +39,36 @@ fun BongaSignIn(
             contentDescription = "")
         Text(text = "Welcome back!")
         Bongatextfield(
-            label = "",
+            label = "E-mail address",
             fieldDescription = "",
-            input = "",
-            hint = "",
-            onChange = {}
+            input = email,
+            hint = "Yourname@gmail.com",
+            onChange = {
+                       email = it
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            ),
+            leading = painterResource(id = R.drawable.image_sec_icon)
         )
         Bongatextfield(
-            label = "",
+            label = "Password",
+            isPassword = true,
             fieldDescription = "",
-            input = "",
-            hint = "",
-            onChange = {}
+            input = password,
+            hint = "Enter your password",
+            onChange = {
+                       password = it
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            trailing = painterResource(id = R.drawable.ic_show_password)
         )
+        Column {
+            Text(text = "Forgot password")
+        }
     }
 }
