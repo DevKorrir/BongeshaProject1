65b9b6122d117b0e9ee10786cc75a9152253c702
DevKorir <kipyegonaldo@proton.me>
2024-11-05 15:05:47 +0300

    change home design

--- app/src/main/java/dev/korryr/bongesha/screens/HomeScreen.kt
+++ app/src/main/java/dev/korryr/bongesha/screens/HomeScreen.kt
@@ -4,15 +4,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
@@ -21,6 +25,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
+import dev.korryr.bongesha.ui.theme.gray01
 import dev.korryr.bongesha.ui.theme.green99
 import dev.korryr.bongesha.viewmodels.CategoryViewModel
 import dev.korryr.bongesha.viewmodels.Product
@@ -45,7 +50,7 @@ fun AllProductsScreen(
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.White)
+            .background(color = gray01)
             .padding(8.dp)
     ) {
         item {
@@ -99,7 +104,8 @@ fun ProductGrid(
 ) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
-        modifier = Modifier.height(500.dp),
+        modifier = Modifier
+            .height(500.dp),
         contentPadding = PaddingValues(8.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
@@ -118,7 +124,10 @@ fun ProductItem(
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .background(Color.LightGray)
+            .background(
+                Color.White,
+                shape = RoundedCornerShape(8.dp)
+            )
             .padding(8.dp)
     ) {
         val imagePainter = rememberImagePainter(data = product.images.firstOrNull())
@@ -126,6 +135,7 @@ fun ProductItem(
             painter =  imagePainter,
             contentDescription = product.name,
             modifier = Modifier
+                .clip(RoundedCornerShape(8.dp))
                 .fillMaxWidth()
                 .height(150.dp),
             contentScale = ContentScale.Crop
